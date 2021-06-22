d873f8d47c34cea213f896c5c108694e95231938
Getinfo_Alexsander <alexsander@getinfo.tech>
2021-06-21 23:01:24 -0400

    Auth ok

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -6,17 +6,18 @@ import googleImg from '../assets/images/google-icon.svg';
 
 import '../style/auth.scss';
 import { Button } from '../components/Button';
-import { auth, firebase } from '../services/firebase';
+import { useContext } from 'react';
+import { AuthContext } from '../App';
 
 export function Home() {
     const history = useHistory();
+    const { user, signInWithGoogle } = useContext(AuthContext);
 
-    function handleCreateRoom() {
-        const provider = new firebase.auth.GoogleAuthProvider();
-        auth.signInWithPopup(provider).then(result => {
-            console.log(result);
-            history.push('/rooms/new')
-        })
+    async function handleCreateRoom() {
+        if (!user) {
+            await signInWithGoogle();
+        }
+        history.push('/rooms/new')
     }
 
     return (
--- src/pages/NewRoom.tsx
+++ src/pages/NewRoom.tsx
@@ -4,8 +4,11 @@ import logoImg from '../assets/images/logo.svg'
 
 import '../style/auth.scss';
 import { Button } from '../components/Button';
+import { useContext } from 'react';
+import { AuthContext } from '../App';
 
 export function NewRoom() {
+  const { user } = useContext(AuthContext);
   return (
     <div id="page-auth">
       <aside>
@@ -16,6 +19,7 @@ export function NewRoom() {
       <main>
         <div className="main-container">
           <img src={logoImg} alt="logo" />
+          <h1>{user.name}</h1>
           <h2>Criar uma nova sala</h2>
           <form>
             <input type="text" placeholder="Nome da sala" />
