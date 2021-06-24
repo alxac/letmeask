ef0bcc30b61df86950bf4454230ba62a0209a477
Getinfo_Alexsander <alexsander@getinfo.tech>
2021-06-23 21:58:23 -0400

    Listagem de Q ok

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,47 +1,68 @@
-import { useHistory } from 'react-router-dom'
+import { useHistory } from "react-router-dom";
 
-import illustrationImg from '../assets/images/illustration.svg'
-import logoImg from '../assets/images/logo.svg'
-import googleImg from '../assets/images/google-icon.svg';
-import '../style/auth.scss';
+import illustrationImg from "../assets/images/illustration.svg";
+import logoImg from "../assets/images/logo.svg";
+import googleImg from "../assets/images/google-icon.svg";
+import "../style/auth.scss";
 
-import { Button } from '../components/Button';
-import { useAuth } from '../hooks/useAuth';
+import { Button } from "../components/Button";
+import { useAuth } from "../hooks/useAuth";
+import { FormEvent, useState } from "react";
+import { database } from "../services/firebase";
 
 export function Home() {
-    const history = useHistory();
-    const { user, signInWithGoogle } = useAuth();
+  const history = useHistory();
+  const [roomCode, setRoomCode] = useState("");
+  const { user, signInWithGoogle } = useAuth();
 
-    async function handleCreateRoom() {
-        if (!user) {
-            await signInWithGoogle();
-        }
-        history.push('/rooms/new')
+  async function handleCreateRoom() {
+    if (!user) {
+      await signInWithGoogle();
+    }
+    history.push("/rooms/new");
+  }
+
+  async function handleJoinRoom(event: FormEvent) {
+    event.preventDefault();
+    if (roomCode.trim() === "") {
+      return;
     }
 
-    return (
-        <div id="page-auth">
-            <aside>
-                <img src={illustrationImg} alt="Img" />
-                <strong>Crie salas</strong>
-                <p>Tire suas duvidas</p>
-            </aside>
-            <main>
-                <div className="main-container">
-                    <img src={logoImg} alt="logo" />
-                    <button onClick={handleCreateRoom} className="create-room">
-                        <img src={googleImg} alt="" />
-                        Crie sua sala com o google
-                    </button>
-                    <div className="separator">ou entre em uma sala</div>
-                    <form>
-                        <input type="text" placeholder="Código sa sala" />
-                        <Button type="submit">
-                            Entrar na sala
-                        </Button>
-                    </form>
-                </div>
-            </main>
+    const roomRef = await database.ref(`rooms/${roomCode}`).get();
+    if (!roomRef.exists()) {
+      alert("Room does not exist.");
+      return;
+    }
+
+    history.push(`/rooms/${roomCode}`);
+  }
+
+  return (
+    <div id="page-auth">
+      <aside>
+        <img src={illustrationImg} alt="Img" />
+        <strong>Crie salas</strong>
+        <p>Tire suas duvidas</p>
+      </aside>
+      <main>
+        <div className="main-container">
+          <img src={logoImg} alt="logo" />
+          <button onClick={handleCreateRoom} className="create-room">
+            <img src={googleImg} alt="" />
+            Crie sua sala com o google
+          </button>
+          <div className="separator">ou entre em uma sala</div>
+          <form onSubmit={handleJoinRoom}>
+            <input
+              type="text"
+              placeholder="Código da sala"
+              onChange={(ev) => setRoomCode(ev.target.value)}
+              value={roomCode}
+            />
+            <Button type="submit">Entrar na sala</Button>
+          </form>
         </div>
-    )
+      </main>
+    </div>
+  );
 }
--- src/pages/NewRoom.tsx
+++ src/pages/NewRoom.tsx
@@ -1,13 +1,33 @@
-import { Link } from 'react-router-dom';
-import illustrationImg from '../assets/images/illustration.svg'
-import logoImg from '../assets/images/logo.svg'
-import '../style/auth.scss';
+import { Link, useHistory } from "react-router-dom";
+import illustrationImg from "../assets/images/illustration.svg";
+import logoImg from "../assets/images/logo.svg";
+import "../style/auth.scss";
 
-import { Button } from '../components/Button';
-import { useAuth } from '../hooks/useAuth';
+import { Button } from "../components/Button";
+import { useAuth } from "../hooks/useAuth";
+import { FormEvent, useState } from "react";
+import { database } from "../services/firebase";
 
 export function NewRoom() {
+  const history = useHistory();
   const { user } = useAuth();
+  const [newRoom, setNewRoom] = useState("");
+
+  async function handleCreateRoom(event: FormEvent) {
+    event.preventDefault();
+
+    if (newRoom.trim() === "") {
+      return;
+    }
+
+    const roomRef = database.ref("rooms");
+    const firebaseRoom = await roomRef.push({
+      title: newRoom,
+      authorId: user.id,
+    });
+    history.push(`/rooms/${firebaseRoom.key}`);
+  }
+
   return (
     <div id="page-auth">
       <aside>
@@ -20,11 +40,14 @@ export function NewRoom() {
           <img src={logoImg} alt="logo" />
           <h1>{user.name}</h1>
           <h2>Criar uma nova sala</h2>
-          <form>
-            <input type="text" placeholder="Nome da sala" />
-            <Button type="submit">
-              Criar sala
-            </Button>
+          <form onSubmit={handleCreateRoom}>
+            <input
+              type="text"
+              placeholder="Nome da sala"
+              onChange={(ev) => setNewRoom(ev.target.value)}
+              value={newRoom}
+            />
+            <Button type="submit">Criar sala</Button>
           </form>
           <p>
             Quer entrar em uma sala? <Link to="/">aqui</Link>
@@ -32,5 +55,5 @@ export function NewRoom() {
         </div>
       </main>
     </div>
-  )
+  );
 }
